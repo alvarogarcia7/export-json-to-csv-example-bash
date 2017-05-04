a0e96b23dc383d5336a1d214ad3e2e7d6c710dd3
Alvaro Garcia <derecerca@gmail.com>
2017-05-04 06:46:40 +0200

    Refactor: parametrize function

--- convert.sh
+++ convert.sh
@@ -8,11 +8,11 @@ function clean_up_fields {
 
 function export_to_csv {
   echo '"F1","F2","F3"'
-  cat result.json|jq -r '.[]|[.f1, .f2, .f3]|@csv'
+  cat $1 |jq -r '.[]|[.f1, .f2, .f3]|@csv'
 }
 
 function extract_status {
   selected_fields $1 > selected_fields.jq
   clean_up_fields selected_fields.jq > result.json
-  export_to_csv > result.csv
+  export_to_csv result.json > result.csv
 }
